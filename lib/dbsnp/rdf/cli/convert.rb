778ef79611f852c490478bb634771dc439a0bac7
Shota Matsumoto <shota.matsumoto@level-five.jp>
2018-04-26 16:30:19 +0900

    add verbose option

--- lib/dbsnp/rdf/cli/convert.rb
+++ lib/dbsnp/rdf/cli/convert.rb
@@ -9,7 +9,8 @@ module DbSNP
 
         DEFAULT_OPTIONS = {
             output_dir: ENV['DATA_DIR'] || ENV['PWD'],
-            help:   false
+            help:   false,
+            verbose: false
         }.freeze
 
         def initialize
@@ -17,6 +18,7 @@ module DbSNP
         end
 
         def run
+          option_parser.parse!
 
           if @options[:help]
             STDERR.puts option_parser.help
@@ -25,7 +27,7 @@ module DbSNP
 
           validate_arguments
 
-          Generator::TurtleGenerator.new(ARGV[0], ARGV[1]).all
+          Generator::TurtleGenerator.new(ARGV[0], ARGV[1], @options).all
 
         rescue OptionParser::InvalidOption => e
           STDERR.puts e.message
@@ -45,24 +47,33 @@ module DbSNP
 
         def option_parser
           OptionParser.new do |op|
-            op.banner = "Usage: #{CLI::PROG_NAME} #{self.class.name.underscore} <src> <dst>\n"
-            op.banner = "Convert dbSNP data to RDF\n"
+            op.banner = "Usage: #{CLI::PROG_NAME} #{self.class.name.demodulize.underscore} <src> <dst>\n"
+            op.banner += "Convert dbSNP data to RDF\n"
 
             op.separator("\nOptions:")
             op.on('-h', '--help', 'show help') do
               @options[:help] = true
+              puts 'help'
+            end
+            op.on('-v', '--verbose [INTERVAL]', OptionParser::DecimalInteger,
+                  'show count of parsed variation lines in progress',
+                  'The output interval can be specified as an integer (equals 100 by default).') do |itv|
+              @options[:verbose] = itv || 100
             end
             op.separator('')
           end
         end
 
         def validate_arguments
-          raise("src and dst must be specified") if ARGV.size < 2
+          verbose = @options[:verbose]
+          raise OptionParser::InvalidOption.new("INTERVAL of verbose must be positive integer") if verbose && verbose <= 0
+
+          raise OptionParser::InvalidOption.new("src and dst must be specified") if ARGV.size < 2
           src, dst = ARGV
-          raise("#{src} must not be a directory.") if File.directory?(src)
-          raise("#{src} does not exist.") unless File.exist?(src)
-          raise("#{dst} does not exist.") unless File.exist?(dst)
-          raise("#{dst} must be a directory.") unless File.directory?(dst)
+          raise OptionParser::InvalidArgument.new("#{src} must not be a directory.") if File.directory?(src)
+          raise OptionParser::InvalidArgument.new("#{src} does not exist.") unless File.exist?(src)
+          raise OptionParser::InvalidArgument.new("#{dst} does not exist.") unless File.exist?(dst)
+          raise OptionParser::InvalidArgument.new("#{dst} must be a directory.") unless File.directory?(dst)
         end
       end
     end
--- lib/dbsnp/rdf/generator/turtle_generator.rb
+++ lib/dbsnp/rdf/generator/turtle_generator.rb
@@ -1,12 +1,14 @@
 module DbSNP::RDF
   module Generator
     class TurtleGenerator
-      def initialize(src_path, dst_path)
+      def initialize(src_path, dst_path, options = {})
         @writer = ::RDF::Writer.for(:turtle)
 
         @src = src_path
 
         @dst = dst_path
+
+        @options = options
       end
 
       def all
@@ -29,10 +31,11 @@ module DbSNP::RDF
           dst_file.write(@writer.buffer(prefixes: DbSNP::RDF::PREFIXES, stream: true) do |buffer|
             # just output header
           end)
-          Parser::VCFParser.open(@src).each do |refsnp|
+          Parser::VCFParser.open(@src).each_with_index do |refsnp, i|
             dst_file.write(@writer.buffer(prefixes: DbSNP::RDF::PREFIXES) do |buffer|
               DbSNP::RDF::Converter::VariationToTriples.convert(refsnp).each { |statement| buffer << statement }
             end.gsub(/^@.*$\n/, ''))
+            puts "parsed #{i + 1} lines..." if @options[:verbose] && ((i + 1) % @options[:verbose]) == 0
           end
         end
       end
