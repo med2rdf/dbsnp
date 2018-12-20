b7a67e7a1f06daa6d8dfa20456a4616fe0df5b0a
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2018-12-20 18:37:24 +0900

    update models

--- lib/dbsnp/rdf/models/variation.rb
+++ lib/dbsnp/rdf/models/variation.rb
@@ -30,7 +30,10 @@ module DbSNP::RDF
       attr_accessor :gene_id_list # array
 
       # @return [Integer, nil] a number of the first dbSNP Build
-      attr_accessor :build
+      attr_accessor :first_build
+
+      # @return [Integer, nil] a number of the latest dbSNP Build
+      attr_accessor :latest_build
 
       # List of items
       # - INDEL
@@ -41,41 +44,12 @@ module DbSNP::RDF
       # @return [String] a variation class
       attr_accessor :variation_class
 
-      # @return [Array<String>] an array (for each alternative allele) of HGVS name
-      attr_accessor :hgvs
-
-      # List of items
-      # - 0: Uncertain significance
-      # - 1: not provided
-      # - 2: Benign
-      # - 3: Likely benign
-      # - 4: Likely pathogenic
-      # - 5: Pathogenic
-      # - 6: drug response
-      # - 8: confers sensitivity
-      # - 9: risk-factor
-      # - 10: association
-      # - 11: protective
-      # - 12: conflict
-      # - 13: affects
-      # - 255: other
-      # @return [Array<Array<Integer>>] an array (for each alternative allele) of an array of clinical significance(s)
-      attr_accessor :clinical_significance
-
       # @return [Hash] an array (for each alternative allele) of alternate allele frequency
       attr_accessor :frequency
 
-      validates :chromosome, :assembly, :position, :rs,
-                :reference_allele, :alternative_alleles,
-                :variation_class, presence: true
-      validate :multi_allele_hgvs, if: Proc.new { |x| x.hgvs.present? }
-      validate :multi_allele_significance, if: Proc.new { |x| x.clinical_significance.present? }
-      validate :multi_allele_frequency, if: Proc.new { |x| x.frequency.present? }
-
-      DBSNP_BASE    = ::RDF::URI.new(PREFIXES[:dbsnp]).freeze
-      GENE_BASE     = ::RDF::URI.new(PREFIXES[:ncbi_gene]).freeze
-      REFSEQ_BASE   = ::RDF::URI.new(PREFIXES[:refseq]).freeze
-      TAXONOMY_BASE = ::RDF::URI.new(PREFIXES[:tax]).freeze
+      # @return [Array<String>] an array of arrays of ClinVar accessions for each alternative allele
+      #   (e.g. `[["RCV000012345.6"], ["RCV000023456.7", "RCV000034567.8"]]`)
+      attr_accessor :clinvar_accession # array
 
       def initialize
         yield self if block_given?
@@ -84,18 +58,16 @@ module DbSNP::RDF
       def to_rdf
         return @graph if @graph
 
-        validate!
-
-        subject = ::RDF::URI.new(BASE_URI % rs)
+        subject = ::RDF::URI.new(PREFIXES[:dbsnp] + rs.match(/\d+/)[0])
         @graph  = ::RDF::Graph.new do |g|
           g << [subject, ::RDF.type, M2R.Variation]
           g << [subject, ::RDF.type, var_class2so(variation_class)]
           g << [subject, ::RDF::Vocab::DC.identifier, rs]
-          g << [subject, ::RDF::Vocab::RDFS.seeAlso, DBSNP_BASE / rs]
-          g << [subject, OBO['RO_0002162'], TAXONOMY_BASE / 9606] # TODO
-          g << [subject, SNPO.build, build] if build
+          g << [subject, SNPO.is_created_in_build, first_build]
+          g << [subject, SNPO.is_updated_in_build, latest_build]
+          g << [subject, M2R.reference_allele, reference_allele]
           gene_id_list&.each do |id|
-            g << [subject, M2R.gene, GENE_BASE / id] # TODO m2r:Gene
+            g << [subject, M2R.gene, ::RDF::URI.new("#{PREFIXES[:ncbigene]}#{id}")]
           end
         end
 
@@ -109,90 +81,60 @@ module DbSNP::RDF
 
       def insert_alleles(parent_subject)
         alternative_alleles.each.with_index do |alt, i|
-          allele = parent_subject / "#{alt}"
+          allele = parent_subject / "##{alt}"
           @graph << [parent_subject, ::RDF::Vocab::DC.hasPart, allele]
           @graph << [allele, ::RDF.type, SNPO.Allele]
+          @graph << [allele, M2R.alternative_allele, alt]
+          @graph << [allele, ::RDF::Vocab::DC.isPartOf, parent_subject]
 
-          if (x = hgvs&.at(i))
-            @graph << [allele, ::RDF::Vocab::RDFS.label, x]
-          end
-
-          if (x = clinical_significance&.at(i))
-            x.each do |j|
-              @graph << [allele, SNPO.clinical_significance, num2significance(j)]
+          if (accessions = clinvar_accession&.at(i))
+            accessions.each do |accession|
+              if (x = accession.match(/^RCV\d+/))
+                @graph << [allele, ::RDF::RDFS.seeAlso, ::RDF::URI.new(PREFIXES[:clinvar] + x[0])]
+              end
             end
           end
 
           frequency&.each do |k, v|
             next if v.at(i).nil?
 
-            frequency = parent_subject / "#{alt}" / 'frequency' / "##{k}"
+            frequency = ::RDF::Node.new
             @graph << [allele, SIO['SIO_000216'], frequency]
-            source = "#{k}Frequency"
-            @graph << [frequency, ::RDF.type, SNPO.respond_to?(source) ? SNPO[source] : SNPO.Frequency]
+            @graph << [frequency, ::RDF.type, ::RDF::Vocab::DC.Frequency]
+            @graph << [frequency, ::RDF::Vocab::DC.source, k]
             @graph << [frequency, ::RDF.value, v.at(i)]
           end
         end
       end
 
       def insert_faldo(parent_subject)
-        loc = parent_subject / "#{assembly}" / 'position'
-        @graph << [parent_subject, FALDO.location, loc]
+        bn_loc = ::RDF::Node.new
+
+        @graph << [parent_subject, FALDO.location, bn_loc]
 
         if reference_allele.length == 1
-          @graph << [loc, ::RDF.type, FALDO.Position]
-          @graph << [loc, ::RDF.type, FALDO.ExactPosition]
-          @graph << [loc, FALDO.position, position]
+          @graph << [bn_loc, ::RDF.type, FALDO.Position]
+          @graph << [bn_loc, ::RDF.type, FALDO.ExactPosition]
+          @graph << [bn_loc, FALDO.position, position]
           if (hco = refseq2hco(chromosome, assembly))
-            @graph << [loc, FALDO.reference, hco]
+            @graph << [bn_loc, FALDO.reference, hco]
           end
-          @graph << [loc, FALDO.reference, REFSEQ_BASE / chromosome]
+          @graph << [bn_loc, FALDO.reference, ::RDF::URI.new("#{PREFIXES[:refseq]}#{chromosome}")]
         else
-          @graph << [loc, ::RDF.type, FALDO.Region]
+          @graph << [bn_loc, ::RDF.type, FALDO.Region]
           %w[begin end].zip([position, position + (reference_allele.length - 1)]).each do |x, pos|
-            node = loc / x
-            @graph << [loc, FALDO[x], node]
-            @graph << [node, ::RDF.type, FALDO.Position]
-            @graph << [node, ::RDF.type, FALDO.ExactPosition]
-            @graph << [node, FALDO.position, pos]
+            bn_pos = ::RDF::Node.new
+            @graph << [bn_loc, FALDO[x], bn_pos]
+            @graph << [bn_pos, ::RDF.type, FALDO.Position]
+            @graph << [bn_pos, ::RDF.type, FALDO.ExactPosition]
+            @graph << [bn_pos, FALDO.position, pos]
             if (hco = refseq2hco(chromosome, assembly))
-              @graph << [node, FALDO.reference, hco]
+              @graph << [bn_pos, FALDO.reference, hco]
             end
-            @graph << [node, FALDO.reference, REFSEQ_BASE / chromosome]
+            @graph << [bn_pos, FALDO.reference, ::RDF::URI.new("#{PREFIXES[:refseq]}#{chromosome}")]
           end
         end
       end
-
-      def multi_allele_hgvs
-        unless alternative_alleles.length == hgvs.length
-          errors.add(:hgvs, "the size of HGVS names differs from alternative alleles")
-        end
-      end
-
-      def multi_allele_significance
-        unless alternative_alleles.length == clinical_significance.length
-          errors.add(:clinical_significance, "the size of clinical significance differs from alternative alleles")
-        end
-      end
-
-      def multi_allele_frequency
-        frequency.each do |k, v|
-          unless alternative_alleles.length == v.length
-            errors.add(:frequency, "the size of #{k} frequency differs from alternative alleles")
-          end
-        end
-      end
-    end
-  end
-end
-
-module ActiveModel
-  class ValidationError
-    def initialize(model)
-      @model = model
-      errors = @model.errors.full_messages.join(", ")
-      errors << ": #{@model.inspect}"
-      super(errors)
     end
   end
 end
